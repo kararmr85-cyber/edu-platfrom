e48e530e308a4857efddd098f07e3b5f4eca52e5
samar <kararmr85@gmail.com>
2026-06-25 07:23:08 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
-  output: 'export',
   images: {
     unoptimized: true,
     remotePatterns: [{ protocol: "https", hostname: "**.supabase.co" }]
+  },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
   }
 };
 
